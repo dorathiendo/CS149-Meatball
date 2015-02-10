1039beaad4b8e5100ec953cbacc298c0f5f5bda6
chorro73 <chorro73@gmail.com>
2015-02-10 12:28:05 -0800

    Update Process Class

--- Assignment 2/src/Process.java
+++ Assignment 2/src/Process.java
@@ -1,26 +1,49 @@
-
+/**
+ * Assignment 2 Simple class that stores the information about a simulated
+ * process.
+ * 
+ * @author Meatball
+ */
 public class Process {
-	private float arrivalTime; //0 through 99 (quanta)
-	private float runTime; //0.1 through 10 quanta
-	private int priority; //1-4, 1 is highest
-	
-	public Process(){
-		//(int)(Math.random()*(max-min+1))+min
-		
+	private String name;
+	private float arrivalTime;
+	private float runTime;
+	private int priority;
+
+	/**
+	 * Constructor that creates a process with:
+	 * -a randomly generated arrival time from 0 to 99
+	 * -a randomly expected run time from 0.1 to 10 quanta
+	 * -a randomly generated priority: either 1, 2, 3, or 4
+	 */
+	public Process(String name) {
+		this.name = name;
 		arrivalTime = (int)(Math.random()*(99-0+1)+0);
 		runTime = (int)(Math.random()*(10-0.1+1)+0.1);
 		priority = (int)(Math.random()*(4-1+1)+1);
 	}
 	
-	public float getArrivalTime(){
+	@Override
+	public String toString() {
+		return "Name: " + name + "\n" +
+				"Arrival Time" + arrivalTime + "\n" +
+				"Expected Run Time" + runTime + "\n" +
+				"Priority" + priority + "\n";
+	}
+	
+	public double getArrivalTime() {
 		return arrivalTime;
 	}
 	
-	public float getRunTime(){
-		return runTime;
+	public String getName() {
+		return name;
 	}
 	
-	public int getPriority(){
+	public int getPriority() {
 		return priority;
+	}
+	
+	public double getRunTime() {
+		return runTime;
 	}
 }
