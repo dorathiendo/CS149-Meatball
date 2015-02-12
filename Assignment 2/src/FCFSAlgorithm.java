4fd4a63818a8276d7230b41c5b0e4d9923754d5d
chorro73 <chorro73@gmail.com>
2015-02-11 21:50:23 -0800

    Sort the ready queue like he asks on slide 16.
    I think the next step is to expand Process class to keep information
    like time remaining, start time, end time, etc.  And make setters and
    getters for them.

--- Assignment 2/src/FCFSAlgorithm.java
+++ Assignment 2/src/FCFSAlgorithm.java
@@ -1,18 +1,23 @@
 import java.util.ArrayList;
-
-
+import java.util.LinkedList;
 public class FCFSAlgorithm implements Algorithm {
-	
+
+	private ArrayList<Process> readyQueue;
+	private LinkedList<String> timeline;
+	private float avgTurnAroundTime;
+	private float avgResponseTime;
+	private float avgArrivalTime;
+
 	/**
 	 * Creates an algorithm object that runs the processes sent through the
 	 * parameter.
+	 * 
 	 * @param processes
 	 */
-	
-	
 	public FCFSAlgorithm(ArrayList<Process> processes) {
-		// TODO Auto-generated constructor stub
+		readyQueue = processes;
 	}
+
 	@Override
 	public int avTurnAroundTime() {
 		// TODO Auto-generated method stub
--- Assignment 2/src/Tester.java
+++ Assignment 2/src/Tester.java
@@ -1,17 +1,27 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class Tester {
 
 	public static void main(String[] args) {
-		ArrayList<Process> processes = new ArrayList<>();
+		ArrayList<Process> readyQueue = new ArrayList<>();
 		int numProcesses = 10; // Will find a better number later
 		char name = 'A';
 		for (int i = 0; i < numProcesses; i++) {
-			processes.add(new Process(Character.toString(name)));
+			readyQueue.add(new Process(Character.toString(name)));
 			name++;
 		}
-
-		System.out.println(processes);
+		// sort queue by arrival time
+		Collections.sort(readyQueue, new Comparator<Process>() {
+			public int compare(Process p1, Process p2) {
+				if (p1.getArrivalTime() == p2.getArrivalTime())
+					return 0;
+				return p1.getArrivalTime() < p2.getArrivalTime() ? -1 : 1;
+			}
+		});
+		
+		FCFSAlgorithm FCFS = new FCFSAlgorithm(readyQueue);
 	}
 
 }
