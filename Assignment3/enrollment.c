dfc705997ee0da26f7f5dfa6ab62748b94347de7
chorro73 <chorro73@gmail.com>
2015-02-23 00:53:41 -0800

    Did stuff together, so no need for notes.
    yay

--- Assignment3/enrollment.c
+++ Assignment3/enrollment.c
@@ -6,15 +6,64 @@
 #include <signal.h>
 #include <sys/time.h>
 
-#define ID_BASE 101;
+#define ID_BASE 101
 
 #define STUDENT_COUNT 75
 #define SECTIONS_COUNT 3
-#define STUDENT_MAX_CAPACITY 20;
+#define STUDENT_MAX_CAPACITY 20
 
-int sections[SECTIONS_COUNT];
-pthread_mutex_t chairMutex;  // mutex protects chairs and wait count
+#define DURATION 120
+
+const char* type[] = {'GS', 'RS', 'EE'};
+int section_enrollment[SECTIONS_COUNT];
+int section_list[SECTIONS_COUNT][STUDENT_MAX_CAPACITY];
+
+pthread_mutex_t section_mutex[SECTIONS_COUNT];
 pthread_mutex_t printMutex;
+
+// The student thread.
+void *student(void *param)
+{
+    int id = *((int *) param);
+
+    // Students will arrive at random times during the office hour.
+    sleep(rand()%DURATION);
+    studentArrives(id);
+
+    return NULL;
+}
+
+void studentArrives(int id)
+{
+    char event[80];
+    arrivalsCount++;
+    char* priority = type[rand()%3];
+    int section = (rand()%4);
+    //section = 3 if student will take any section
+    if (section == 3){
+        if(section_enrollment[0] < STUDENT_MAX_CAPACITY)
+            section = 0;
+        else if(section_enrollment[1] < STUDENT_MAX_CAPACITY)
+            section = 1;
+        else
+            section = 2;
+    }
+
+    int enrollment = section_enrollment[section];
+    if (enrollment < STUDENT_MAX_CAPACITY) {
+        pthread_mutex_lock(&section_mutex[section]);
+        section_list[section][enrollment] = id;
+        section_enrollment[section]++;
+
+        sprintf(event, "Student %d arrives and waits", id);
+        print(event);
+    }
+    else {
+        leavesCount++;
+        sprintf(event, "Student %d arrives and leaves", id);
+        print(event);
+    }
+}
 
 int main(int argc, char *argv[]){
 	int students[STUDENT_COUNT];
